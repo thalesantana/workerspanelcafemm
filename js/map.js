2a9a3369fbeb36bd23c23b30e1c6e50f67af4a42
Suspir0n <evandrojunior1615@gmail.com>
2021-01-31 10:12:41 -0300

    Implemented but marker at map with the count of dev

--- js/map.js
+++ js/map.js
@@ -1,13 +1,19 @@
+var data = '[{"lat": -13.103920724367565,"long": -52.10791325712833, "people_number": 4}, {"lat": -12.971688847390759,"long": -38.501132213857524, "people_number": 7}, {"lat": -22.901103216851517,"long": -43.17305053572673, "people_number": 5}, {"lat": -19.903952721886068,"long": -43.935221372850194, "people_number": 15}]'
+var data_lat_long = JSON.parse(data);
+
 function inicializar() {
-    var map = L.map('map').setView([-22.912869, -43.228963], 17);
-      mapLink = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
-      L.tileLayer(
-          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: 'Map data &copy; ' + mapLink,
-          maxZoom: 3,
-          title: '2 Desenvolvedores'
-          }).addTo(map);
-          var marker = L.marker([-22.912869, -43.228963]).addTo(map);
+  var map = L.map('map').setView([data_lat_long[0].lat, data_lat_long[0].long], 4);
+    mapLink = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+    L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; ' + mapLink,
+        maxZoom: 16,
+        title: '2 Desenvolvedores'
+        }).addTo(map);
+        for (var i = 1; i < data_lat_long.length; i++){
+          var marker = L.marker([data_lat_long[i].lat, data_lat_long[i].long]).addTo(map);
+          marker.bindTooltip(data_lat_long[i].people_number + " Desenvolvedores", { permanent: false, offset: [0, 12] });
+        }
 }
 
 inicializar();
